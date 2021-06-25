63fa296c97b08ccfd4272ef8ed4e74564aefd125
SorinTautu1991 <sorin.tautu@gmail.com>
2021-06-25 12:14:48 +0300

    Logout done

--- src/infrastructure/navigation/app-navigator.js
+++ src/infrastructure/navigation/app-navigator.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
+import { AuthenticationContext } from "../../services/authentication/authentication-context";
 
 import { SafeArea } from "../../components/utility/safe-area.comp";
 
@@ -17,12 +18,15 @@ const TAB_ICON = {
   Settings: "md-settings",
 };
 
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-
+const Settings = () => {
+  const { onLogout } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+      <Button title="Logout" onPress={() => onLogout()} />
+    </SafeArea>
+  );
+};
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
   return {
